eade97a8a567329aa5109471394992440192c7db
vince <vincedinardo21@gmail.com>
2022-06-27 15:39:08 +0200

    Snack 1

--- Snack1/index.php
+++ Snack1/index.php
@@ -1,3 +1,47 @@
+<?php
+    $partite = [
+        //Partita 1
+        [
+            [
+                "nome"      => "Olimpia Milano",
+                "casa"      => true,
+                "punteggio" => 55,
+            ],
+            [
+                "nome"      => "Napoli",
+                "casa"      => false,
+                "punteggio" => 78,
+            ],
+        ],
+        //Partita 2
+        [
+            [
+                "nome"      => "Roma",
+                "casa"      => true,
+                "punteggio" => 101,
+            ],
+            [
+                "nome"      => "Torino",
+                "casa"      => false,
+                "punteggio" => 89,
+            ],
+        ],
+        //Partita 3
+        [
+            [
+                "nome"      => "Pescara",
+                "casa"      => true,
+                "punteggio" => 45,
+            ],
+            [
+                "nome"      => "Bologna",
+                "casa"      => false,
+                "punteggio" => 97,
+            ],
+        ],
+    ];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +51,12 @@
     <title>Snack1</title>
 </head>
 <body>
-    testo
+    <?php
+        $partiteLength = count($partite);
+        for($i = 0; $i < $partiteLength; $i++){
+            $partita = $partite[$i];
+            echo "<div>{$partita[0]['nome']} - {$partita[1]['nome']} | {$partita[0]['punteggio']} - {$partita[1]['punteggio']}<div/>";
+        } 
+    ?>
 </body>
 </html>
