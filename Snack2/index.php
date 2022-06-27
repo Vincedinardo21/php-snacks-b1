931b5e472e4e7c6433a1c34ec9294cc627eadeb0
vince <vincedinardo21@gmail.com>
2022-06-27 16:33:39 +0200

    Snack 2 eta e nome

---
+++ Snack2/index.php
@@ -0,0 +1,31 @@
+<?php
+    $name = $_GET["name"];
+    $email = $_GET["email"];
+    $age = $_GET["age"];
+
+    $numberChars = strlen($name);
+    if($numberChars > 3 || is_numeric($age)){
+        $result = "Accesso consentito";
+    } else {
+        $result = "Accesso NON consentito";
+    }
+
+    var_dump($numberChars);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack2</title>
+</head>
+<body>
+    <h2>Nome: <?= $name ?></h2>
+    <h2>Email: <?= $email ?></h2>
+    <h2>Età: <?= $age ?></h2>
+
+    <h3><?= $result ?></h3>
+</body>
+</html>
